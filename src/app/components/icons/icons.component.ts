0b34923f5498d6914bbbfacfd140a608b7e1b5e7
Ndivho <ndibo69@gmail.com>
2025-02-18 18:44:59 +0200

    Added homepage

---
+++ src/app/components/icons/icons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icons',
+  standalone: true,
+  imports: [],
+  templateUrl: './icons.component.html',
+  styleUrl: './icons.component.scss'
+})
+export class IconsComponent {
+
+}
