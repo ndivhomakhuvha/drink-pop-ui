c5e74ebfc252c9f292bd0f909baf26cdb88cdedd
Ndivho <ndibo69@gmail.com>
2025-02-21 15:53:18 +0200

    prod

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,4 @@
-export const baseUrl: string = 'http://localhost:8080';
+export const baseUrl: string = 'http://138.68.150.71:8080';
 export const productsEndpoints = {
   getAllProducts: `${baseUrl}/products/all`,
 };
@@ -6,6 +6,6 @@ export const storeEndpoints = {
   getAllStores: `${baseUrl}/stores`,
 };
 
-export const waitListEndpoints ={
-  signUpForWaitList: `${baseUrl}/waitlist/`
-}
+export const waitListEndpoints = {
+  signUpForWaitList: `${baseUrl}/waitlist/`,
+};
